06e006eda0c4d99f1dab1362d1af73b2b4eadd3e
theemadnes <alex.mattson@gmail.com>
2015-01-29 10:54:57 -0800

    flashing cube

--- pogl2.py
+++ pogl2.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 from pygame.locals import *
 from OpenGL.GL import * 
 from OpenGL.GLU import * 
@@ -39,13 +40,49 @@ edges = (
 
 def Cube():
 
-    glBegin(GL_LINES)
+    glBegin(GL_QUADS)
+    
+
+    for surface in surfaces:
+        #x=0
+        for vertex in surface:
+            x = randint(0,len(colors)-1)
+            glColor3fv(colors[x])
+            glVertex3fv(vertices[vertex])
+
+    glEnd()
+
+    glBegin(GL_LINES) # used to encase our drawing stuff
     
     for edge in edges:
         for vertex in edge:
             glVertex3fv(vertices[vertex])
 
     glEnd()
+
+surfaces = (
+
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+
+    )
+
+colors = (
+
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,0,0),
+    (1,0,1),
+    (0,1,1),
+    (1,1,0),
+    (1,1,1)
+
+    )
 
 def main():
 
@@ -66,6 +103,7 @@ def main():
                 pygame.quit()
                 quit()
 
+        glRotatef(1,3,1,1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) # clears the slate
         Cube()
         pygame.display.flip()
